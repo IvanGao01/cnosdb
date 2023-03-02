6c25cc6ecb50e6853ac0877bb2f93fa8ef48e64a
肖明浩 <88817514+xmh1011@users.noreply.github.com>
2023-03-03 00:04:08 +0800

    chore: update musl

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -o errexit
 
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt-get update
+apt-get install --assume-yes apt-utils
 apt-get install -y wget
 apt-get install -y git
 apt-get install -y unzip
@@ -12,8 +14,8 @@ deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-12 main
 deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-12 main
 EOF
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key| apt-key add -
-apt-get update
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key > apt-key.txt
+apt-key add apt-key.txt
 apt-get install -y libclang1-12
 apt-get install -y llvm-12
 
